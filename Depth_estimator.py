84b2f7ca94ec47a2b4b073d4d6ff13c7afba1534
Diyari-Fariq-M-salih <diyari.m.salih@gmail.com>
2025-11-11 13:32:20 +0100

    loading images
    need to calibrate the camera

--- Depth_estimator.py
+++ Depth_estimator.py
@@ -5,3 +5,17 @@ Compute disparity and approximate depth using stereo images.
 Before running:
   pip install opencv-python numpy
 """
+
+import cv2
+import numpy as np
+
+left_image_path = "left.jpg"
+right_image_path = "right.jpg"
+
+# need to calibrate the stereo camera first to get the calculation parameters
+# Z = (f * B) / d
+# Where:
+# Z = depth (distance to object)
+# f = focal length of the camera
+# B = baseline (distance between cameras)
+# d = disparity (difference in pixel coordinates of the same point in both images)
